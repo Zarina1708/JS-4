e2a4e41c69783da849409952022a44e9193094f6
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2022-12-12 00:42:57 +0600

    dz-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
+import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//     </div>
-//   );
-// }
-
-
-function MyInfo(props) {
+function App() {
   return (
-    <div classname="MyInfo">
-      <h1 >{props.name}</h1>
-      <p>My name is Zarina, I am 20 years old and I am a 3rd year student at KNU. I was born in Kazakhstan in the capital Astana. I take courses in JavaScript programming. I have been studying for 4 months already, I want to master this JS language and find a job...</p>
-      <ul><li>New York</li></ul>
-      <ul><li>Switzerland</li></ul>
-      <ul><li>Paris</li></ul>
+    <div className="App">
+      <h1>Hello world!</h1>
+      <input></input>
     </div>
-  )
+  );
 }
 
+class BuyList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for{this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
-export default MyInfo;
+export default BuyList;
